42d4e43b0258c8b4e676d561e119a848385be3a7
Adam Szymanowski <adam.szym@yahoo.pl>
2021-01-16 23:01:13 +0100

    Open Window and handle the close/destroy.

--- README.md
+++ README.md
@@ -9,12 +9,12 @@ More info at their [github repo](https://github.com/RayTracing/raytracing.github
 The idea is to do the book, but using Win32 Window as a renderer.
 
 I want to utilize simple win32 code from Handmade Hero.
-[Handmade Hero Day 002](https://www.youtube.com/watch?v=4ROiWonnWGk&t=5s)
+- [Handmade Hero Day 002](https://www.youtube.com/watch?v=4ROiWonnWGk&t=5s)
 
 But I want to use just Visual Studio in the process.
-NOTE: There, were problems... solutions below!
 
-# Help on Visual Studio setup
+NOTE: There, were problems... solutions below!
+# Documentation
 
 ## Entry point errors
 Avoiding dumb
@@ -29,4 +29,20 @@ WinMainCRTStartup
 
 [/ENTRY (Entry-Point Symbol)](https://docs.microsoft.com/en-us/cpp/build/reference/entry-entry-point-symbol?redirectedfrom=MSDN&view=msvc-160)
 
-## next
+## error C2440
+cannot convert from 'const char [12]' to 'LPCWSTR'
+
+RayTracingInOneWeekend -> Properties ->General -> Character Set -> (choose value):
+Use Multi-Byte Character Set
+
+## Other
+[Window Class Styles](https://docs.microsoft.com/en-us/windows/win32/winmsg/window-class-styles)
+
+### System defined messages (in WindowProc)
+[WindowProc callback function](https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms633573(v=vs.85))
+[About Messages and Message Queues](https://docs.microsoft.com/pl-pl/windows/win32/winmsg/about-messages-and-message-queues?redirectedfrom=MSDN)
+[Window Notifications](https://docs.microsoft.com/pl-pl/windows/win32/winmsg/window-notifications)
+
+### CreateWindow
+[CreateWindowExA function (winuser.h)](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexa)
+[Window Styles](https://docs.microsoft.com/en-us/windows/win32/winmsg/window-styles)
--- Win32.cpp
+++ Win32.cpp
@@ -1,26 +1,111 @@
 #include <windows.h>
 
+LRESULT CALLBACK 
+MainWindowCallback(
+	HWND   Window,
+	UINT   Message,
+	WPARAM WParam,
+	LPARAM LParam)
+
+{
+	LRESULT Result = 0;
+	switch (Message)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugStringA("WM_SIZE");
+		} break;
+
+		case WM_CLOSE:
+		{
+			DestroyWindow(Window);
+		} break;
+
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			Result = 0;
+		} break;
+
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WM_ACTIVATEAPP");
+		} break;
+
+		default:
+		{
+			OutputDebugStringA("default\n");
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		}
+	}
+
+	return Result;
+}
 
 int WINAPI WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR     lpCmdLine,
-	int       nShowCmd)
+	HINSTANCE Instance,
+	HINSTANCE PrevInstance,
+	LPSTR     CommandLine,
+	int       ShowCode)
 {
-	MessageBoxA(0, "This is Win32 Window.", "Win32 Window", MB_OK | MB_ICONINFORMATION);
-	/*
 	WNDCLASS WindowClass = {};
 	
-	WindowClass.style = ;
-	WindowClass.lpfnWndProc = ;
-	WindowClass.cbClsExtra = ;
-	WindowClass.cbWndExtra = ;
-	WindowClass.cbWndExtra = ;
-	WindowClass.hInstance = ;
-	WindowClass.hIcon = ;
-	WindowClass.hCursor = ;
-	WindowClass.hbrBackground = ;
-	WindowClass.lpszMenuName = ;
-	WindowClass.lpszClassName = ;
-	*/
+	WindowClass.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
+	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.hInstance = Instance;
+	//WindowClass.hIcon;
+	WindowClass.lpszClassName = "RTIOW_Class";
+
+	
+
+	if (RegisterClass(&WindowClass))
+	{
+		HWND WindowHandle = CreateWindowExA(
+			0,
+			WindowClass.lpszClassName,
+			"Ray Tracing In One Weekend",
+			WS_OVERLAPPEDWINDOW,
+			0,
+			0,
+			800, // width
+			600, // height
+			0,
+			0,
+			Instance,
+			0
+		);
+
+		if (WindowHandle)
+		{
+			// This need to be a part of code for some reason
+			ShowWindow(WindowHandle, ShowCode);
+
+			MSG Message;
+			for(;;)
+			{
+				
+
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				if (MessageResult > 0)
+				{
+					TranslateMessage(&Message);
+					DispatchMessageA(&Message);
+				}
+				else
+				{
+					break;
+				}
+
+			}
+
+		}
+		else
+		{
+			// TODO: Logging
+		}
+	}
+	else
+	{
+		// TODO: Logging
+	}
 }
