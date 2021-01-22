e7b709a1ce264d984a64e97af0d70b2726581517
Adam Szymanowski <adam.szym@yahoo.pl>
2021-01-22 09:30:22 +0100

    Chapter 9: Dielectrics, Done.

--- Win32.cpp
+++ Win32.cpp
@@ -63,16 +63,24 @@ refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted)
 {
 	vec3 uv = unit_vector(v);
 	float dt = dot(uv, n);
-	float discriminant = 1.0f - ni_over_nt*ni_over_nt * (1.0f-dt*dt) ;
+	float discriminant = 1.0f - (ni_over_nt*ni_over_nt * (1.0f-dt*dt)) ;
 	if (discriminant > 0)
 	{
-		refracted = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
+		refracted = (ni_over_nt * (uv - (n * dt))) - n * sqrt(discriminant);
 		return true;
 	}
 	else
 	{
 		return false;
 	}
+}
+
+internal_function float
+schlick(float cosine, float ref_idx)
+{
+	float r0 = (1 - ref_idx) / (1 + ref_idx);
+	r0 = r0 * r0;
+	return r0 + (1 - r0) * pow((1 - cosine), 5);
 }
 
 class dielectric : public material
@@ -86,26 +94,38 @@ public:
 		float ni_over_nt;
 		attenuation = vec3(1.0, 1.0, 1.0);
 		vec3 refracted;
+		float reflect_prob;
+		float cosine;
 
 		if (dot(r_in.direction(), rec.normal) > 0)
 		{
 			outward_normal = -rec.normal;
 			ni_over_nt = ref_idx;
+			cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
 		}
 		else
 		{
 			outward_normal = rec.normal;
 			ni_over_nt = 1.0f / ref_idx;
+			cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
 		}
 
 		if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted))
+		{
+			reflect_prob = schlick(cosine, ref_idx);
+		}
+		else
+		{
+			reflect_prob = 1.0f;
+		}
+
+		if (random_float() < reflect_prob)
 		{
 			scattered = ray(rec.p, reflected);
 		}
 		else
 		{
-			scattered = ray(rec.p, reflected);
-			return false;
+			scattered = ray(rec.p, refracted);
 		}
 
 		return true;
@@ -195,12 +215,13 @@ Win32ResizeDIBSection()
 	BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 	
 	// Ray Tracing In One Weekend Rendering (setup)
-	hitable* list[4];
+	hitable* list[5];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5f, new lambertian(vec3(0.1f, 0.2f, 0.5f)));
 	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8f, 0.8f, 0.0f)));
 	list[2] = new sphere(vec3(1, 0, -1), 0.5f, new metal(vec3(0.8f, 0.6f, 0.2f), 0.3f));
 	list[3] = new sphere(vec3(-1, 0, -1), 0.5f, new dielectric(1.5f));
-	hitable* world = new hitable_list(list, 4);
+	list[4] = new sphere(vec3(-1, 0, -1), -0.45f, new dielectric(1.5f));
+	hitable* world = new hitable_list(list, 5);
 	camera cam;
 	// Ray Tracing In One Weekend Rendering (setup END)
 
